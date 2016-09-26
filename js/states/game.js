4e157781a141df15e3e0c131a9369272312d433b
Gustaf Elbander <gelbander@gmail.com>
2016-09-26 16:02:01 +0200

    And some action

--- js/states/game.js
+++ js/states/game.js
@@ -149,10 +149,19 @@ var gameState = (function() {
         }
     }
 
+    function changeBackground() {
+        var c = Phaser.Color.getRandomColor(50, 255, 255);
+
+        game.stage.backgroundColor = c;
+
+    }
+
     function handleCollisions() {
 
         function handlePlayerHitFruit() {
             fruit.kill();
+
+            changeBackground();
 
             addBodyPart = true;
         }
